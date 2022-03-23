4d94e9d7fe05a64f59626a0480a690e4f69bfe17
RunningBeef <2723772192@qq.com>
2022-03-23 19:49:25 +0800

    互斥锁头文件

--- include/Mutex.h
+++ include/Mutex.h
@@ -4,5 +4,33 @@
 
 #ifndef RUNNINGBEEF_WEBSERVER_MUTEX_H
 #define RUNNINGBEEF_WEBSERVER_MUTEX_H
+#include "../include/Noncopyable.h"
+#include<pthread.h>
+
+/*继承Noncopyable类防止锁被无意间拷贝，
+ * 给一个已经上锁的锁在次上锁，
+ * 或者一个已经解锁的锁解锁
+ * 将导致不可预期的后果
+ * 因此我们delte了Mutex的拷贝函数和'='重载
+ * 并且对锁的解锁和开锁和类的生命周期绑定在一起
+ **/
+class Mutex : public Noncopyable{
+public:
+    Mutex();
+    ~Mutex();
+    void lock();
+    void unlock();
+    pthread_mutex_t * getMutex();
+public: pthread_mutex_t mutex_;
+};
+
+/*对锁的解锁和开锁和类的生命周期绑定在一起*/
+class MutexGuard{
+public:
+    explicit MutexGuard(Mutex & mutex);
+    ~MutexGuard();
+public:
+    Mutex &mutex_;
+};
 
 #endif //RUNNINGBEEF_WEBSERVER_MUTEX_H
