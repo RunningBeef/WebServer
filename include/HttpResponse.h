b46e37a990219e4aeec02b491c52aadfedb11264
RunningBeef <2723772192@qq.com>
2022-03-25 20:10:18 +0800

    Http响应报文类

--- include/HttpResponse.h
+++ include/HttpResponse.h
@@ -5,4 +5,107 @@
 #ifndef RUNNINGBEEF_WEBSERVER_HTTPRESPONSE_H
 #define RUNNINGBEEF_WEBSERVER_HTTPRESPONSE_H
 
+#include <iostream>
+#include <string>
+#include "HttpRequest.h"
+
+struct MimeType {
+    MimeType(const std::string &str) : type(str) {};
+
+    MimeType(const char *str) : type(str) {};
+
+    std::string type;
+};
+
+extern std::unordered_map<std::string, MimeType> Mime_map;
+
+class HttpResponse {
+
+public:
+    enum HttpStatusCode {
+        Unknow,
+        k200Ok = 200,
+        k403forbiden = 403,
+        k404NotFound = 404
+    };
+
+    explicit HttpResponse(bool mkeep = true)
+            : mStatusCode(Unknow), keep_alive_(mkeep), mMime("text/html"), mBody(nullptr),
+              mVersion(HttpRequest::HTTP1_1) {}
+
+    void setStatusCode(HttpStatusCode code) {
+        mStatusCode = code;
+    }
+
+    void setBody(const char *buf) {
+        mBody = buf;
+    }
+
+    void setContentLength(int len) {
+        mContentLength = len;
+    }
+
+    void setVersion(const HttpRequest::HTTP_VERSION &version) {
+        mVersion = version;
+    }
+
+    void setStatusMsg(const std::string &msg) {
+        mStatusMsg = msg;
+    }
+
+    void setFilePath(const std::string &path) {
+        mFilePath = path;
+    }
+
+    void setMime(const MimeType &mime) {
+        mMime = mime;
+    }
+
+    void setKeepAlive(bool isalive) {
+        keep_alive_ = isalive;
+    }
+
+    void addHeader(const std::string &key, const std::string &value) {
+        mHeaders[key] = value;
+    }
+
+    bool keep_alive() const {
+        return keep_alive_;
+    }
+
+    const HttpRequest::HTTP_VERSION version() const {
+        return mVersion;
+    }
+
+    const std::string &filePath() const {
+        return mFilePath;
+    }
+
+    HttpStatusCode statusCode() const {
+        return mStatusCode;
+    }
+
+    const std::string &statusMsg() const {
+        return mStatusMsg;
+    }
+
+    void appenBuffer(char *) const;
+
+    ~HttpResponse() {
+        if (mBody != nullptr)
+            delete[] mBody;
+    }
+
+private:
+    HttpStatusCode mStatusCode;/*状态码*/
+    HttpRequest::HTTP_VERSION mVersion;
+    std::string mStatusMsg;/*状态原因短语*/
+    bool keep_alive_;/*长链接*/
+    MimeType mMime;/*媒体类型*/
+    const char *mBody;/*返回给客户端的媒体信息*/
+    int mContentLength;
+    std::string mFilePath;
+    std::unordered_map<std::string, std::string> mHeaders;
+};
+
 #endif //RUNNINGBEEF_WEBSERVER_HTTPRESPONSE_H
--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -2,3 +2,50 @@
 // Created by yonggeng on 3/25/22.
 //
 
+#include "../include/HttpResponse.h"
+
+std::unordered_map<std::string, MimeType> Mime_map = {
+        {".html", "text/html"},
+        {".xml", "text/xml"},
+        {".xhtml", "application/xhtml+xml"},
+        {".txt", "text/plain"},
+        {".rtf", "application/rtf"},
+        {".pdf", "application/pdf"},
+        {".word", "application/msword"},
+        {".png", "image/png"},
+        {".gif", "image/gif"},
+        {".jpg", "image/jpeg"},
+        {".jpeg", "image/jpeg"},
+        {".au", "audio/basic"},
+        {".mpeg", "video/mpeg"},
+        {".mpg", "video/mpeg"},
+        {".avi", "video/x-msvideo"},
+        {".gz", "application/x-gzip"},
+        {".tar", "application/x-tar"},
+        {".css", "text/css"},
+        {"", "text/plain"},
+        {"default","text/plain"}
+};
+
+void HttpResponse::appenBuffer(char *buffer) const{
+
+    // 版本
+    sprintf(buffer, "%s %d %s\r\n", mVersion,mStatusCode, mStatusMsg.c_str());
+
+    // 头部字段
+    for (auto it = mHeaders.begin(); it != mHeaders.end(); it++) {
+        sprintf(buffer, "%s%s: %s\r\n", buffer, it->first.c_str(), it->second.c_str());
+    }
+    sprintf(buffer, "%sContent-type: %s\r\n", buffer, mMime.type.c_str());
+    // keep_alive
+    if (keep_alive_) {
+        sprintf(buffer, "%sConnection: keep-alive\r\n", buffer);
+    } else {
+        sprintf(buffer, "%sConnection: close\r\n", buffer);
+    }
+}
+
+
+
+
+
