93e47d3fc9f9808b4067046e0393b6f7bbcaf4af
RunningBeef <2723772192@qq.com>
2022-03-25 20:13:47 +0800

    定时器及定时器管理类

--- include/Timer.h
+++ include/Timer.h
@@ -4,5 +4,54 @@
 
 #ifndef RUNNINGBEEF_WEBSERVER_TIMER_H
 #define RUNNINGBEEF_WEBSERVER_TIMER_H
+#include "HttpData.h"
+#include "Mutex.h"
+#include <queue>
+#include <deque>
+#include<memory>
 
+class TimerNode{
+public:
+    TimerNode(size_t interval,shared_ptr<HttpData> httpData,void (*cbFunc)(std::shared_ptr<HttpData> ));
+    ~TimerNode();
+    bool isExpired();/*是否超时*/
+    bool isDeleted();/*是否因为异常或者连接更新，被标记为删除*/
+
+    void setExpiredTime(size_t interval);/*设置过期时间*/
+
+    size_t getExpiredTime();/*获取过期时间*/
+
+    void setDeleted();/*惰性删除标记*/
+
+    static void updateCurrentTime();/*更新系统当前时间*/
+
+public:
+    static size_t current_src;/*系统当前时间*/
+    static size_t DEFAULT_INTERVAL_SEC;/*默认超时时间*/
+    void (* cbFunc_ )(std::shared_ptr<HttpData> );/*回调函数*/
+    std::shared_ptr<HttpData> httpData_;/*socket连接数据*/
+
+private:
+    bool deleted_;/*惰性删除标记*/
+    size_t expiredTime_;/*超时时间*/
+
+};
+
+struct TimerCmp{
+    bool operator()(std::shared_ptr<TimerNode> & a,std::shared_ptr<TimerNode> & b)const {
+        return a->getExpired() < b->getExpired();
+    }
+};
+/*定时器容器*/
+class TimerManager{
+public:
+
+    /*给堆添加定时器*/
+    void addTimerNode(std::shared_ptr<HttpData> & httpData,size_t interval,void (* cbFunc )(std::shared_ptr<HttpData>));
+    void tick();
+
+private:
+    Mutex mutex_;
+    std::priority_queue<std::shared_ptr<TimerNode>, std::vector<std::shared_ptr<TimerNode>>, TimerCmp> timerQueue;
+};
 #endif //RUNNINGBEEF_WEBSERVER_TIMER_H
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,4 +1,74 @@
 //
 // Created by yonggeng on 3/25/22.
 //
+#include "../include/Timer.h"
+#include <sys/time.h>
+
+size_t TimerNode:: DEFAULT_INTERVAL_SEC = 20;
+TimerNode::TimerNode(size_t interval,std::shared_ptr<HttpData> httpData,void (*cbFunc)(std::shared_ptr<HttpData>))
+:httpData_(httpData),deleted_(false),cbFunc_(cbFunc){
+    TimerNode::setExpiredTime(TimerNode::DEFAULT_INTERVAL_SEC);
+}
+
+/*调用回调函数关闭socket...等资源*/
+TimerNode::~TimerNode(){
+    if(httpData_){
+        cbFunc_(httpData_);
+    }
+}
+
+/*是否超时*/
+bool TimerNode::isExpired(){
+    return expiredTime_ < TimerNode::current_src;
+}
+
+/*是否因为异常或着连接更新，被标记为删除*/
+bool TimerNode:: isDeleted(){
+    return deleted_;
+}
+
+/*设置过期时间*/
+void TimerNode::setExpiredTime(size_t interval = DEFAULT_INTERVAL_SEC){
+    updateCurrentTime();
+    TimerNode::expiredTime_ = TimerNode::current_src + interval;
+}
+
+size_t TimerNode::getExpiredTime() {
+    return expiredTime_;
+}
+
+void TimerNode::setDeleted(){
+    deleted_ = true;
+}
+
+/*更新系统当前时间*/
+void TimerNode::updateCurrentTime(){
+    struct  timeval cur;
+    gettimeofday(&cur,NULL);
+    TimerNode::current_src = cur.tv_sec;
+}
+
+
+void TimerManager::addTimerNode(std::shared_ptr<HttpData> & httpData,size_t interval,void (* cbFunc )(std::shared_ptr<HttpData>)) {
+    std::shared_ptr<TimerNode> timerNode(new TimerNode(interval,httpData,cbFunc));
+    {
+        MutexGuard mutexGuard(mutex_);/*上锁*/
+        timerQueue.push(timerNode);
+        httpData->setTimerNode(timerNode);
+    }
+}
+
+void TimerManager::tick() {
+    TimerNode::updateCurrentTime();
+    MutexGuard mutexGuard(mutex_);/*上锁*/
+    while(!timerQueue.empty()){
+        std::shared_ptr<TimerNode> temp = timerQueue.top();
+        if(temp->isExpired() || temp->isDeleted())
+        {
+            timerQueue.pop();
+        }else break;
+    }
+}
+
+
 
