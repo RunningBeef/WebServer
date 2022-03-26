173e4997140e4c5d4bec8efb85e318ced4f5e5df
RunningBeef <2723772192@qq.com>
2022-03-26 20:00:50 +0800

    epoll函数封装

--- include/Epoll.h
+++ include/Epoll.h
@@ -4,5 +4,18 @@
 
 #ifndef RUNNINGBEEF_WEBSERVER_EPOLL_H
 #define RUNNINGBEEF_WEBSERVER_EPOLL_H
+#include <sys/epoll.h>
 
+class Epoll{
+public:
+    Epoll(int size);
+    ~Epoll();
+    int modFd(int op,int fd,struct epoll_event *);/**/
+    int wait(struct epoll_event * ,int timeout);/*调用epoll_wait*/
+    static  __uint32_t DEFAULT_EVENT;/*默认监听事件*/
+private:
+    int max_events;/*就绪事件列表大小*/
+    int epoll_fd;/*保存epoll_create()返回的内核事件表文件描述符*/
+    struct epoll_event * events;/*保存监听到的就绪事件*/
+};
 #endif //RUNNINGBEEF_WEBSERVER_EPOLL_H
--- src/Epoll.cpp
+++ src/Epoll.cpp
@@ -2,3 +2,25 @@
 // Created by yonggeng on 3/25/22.
 //
 
+#include "../include/Epoll.h"
+
+__uint32_t Epoll:: DEFAULT_EVENT = EPOLLIN | EPOLLET | EPOLLONESHOT;
+
+Epoll::Epoll(int size):events(nullptr){
+    max_events = 1024 > size ? 1024 : size;/*设置监听事件数组大小*/
+    epoll_fd = epoll_create(max_events);
+    events = new epoll_event[max_events];
+}
+Epoll::~Epoll() {
+    if (events != nullptr)
+        delete events;
+}
+
+int Epoll::modFd(int op,int fd,struct epoll_event *event){
+    return epoll_ctl(epoll_fd,op,fd,event);
+}
+
+int Epoll::wait(struct epoll_event * events ,int timeout){
+
+    return epoll_wait(epoll_fd,events,max_events,timeout);
+}
