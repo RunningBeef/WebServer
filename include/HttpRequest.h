e5bac4a91295b525a05124e54f3d2093d36c0db0
RunningBeef <2723772192@qq.com>
2022-03-28 14:41:04 +0800

    Http请求类

--- include/HttpRequest.h
+++ include/HttpRequest.h
@@ -7,11 +7,13 @@
 #include<iostream>
 #include<unordered_map>
 
+
 class HttpRequest;
 std::ostream & operator << (std::ostream & ,const HttpRequest &);/*输出整理好的Http请求报文的各个字段*/
 
 class HttpRequest{
     friend std::ostream & operator << (std::ostream &,const HttpRequest & );
+
 public:
     enum HTTP_VERSION{/*HTTP版本*/
         HTTP1_0 = 0,HTTP1_1,VERSION_NOT_SPORT
@@ -29,7 +31,9 @@ public:
         Cache_Control,
         Upgrade_Insecure_Requests
     };
-    static std::unordered_map<std::string, HTTP_HEADER> header_map;
+
+
+    static std::unordered_map<std::string, HTTP_HEADER> static_stringToHeader_map_;
     struct EnumClassHash {
         template<typename T>
         std::size_t operator()(T t) const {
@@ -38,13 +42,27 @@ public:
     };
 
 public:
+    void setMethod(HTTP_METHOD method){method_ = method;}
+    HTTP_METHOD getMethod(){return method_;}
+
+    void setUrl(std::string url){url_ = url;}
+    std::string getUrl(){return url_;}
+
+    void setVersion(HTTP_VERSION version){version_ = version;}
+    HTTP_VERSION getVersion(){return version_;}
+
+    void setContent(std::string content){content_ = content_;}
+    std::string getContent(){return content_;}
+
+public:
     HttpRequest(HTTP_METHOD method = METHOD_NOT_SPORT,std::string url = "",HTTP_VERSION version = VERSION_NOT_SPORT):
     method_(method),url_(url),version_(version),content_(""){};
+public:/*有空改成private*/
     HTTP_METHOD method_;
     std::string url_;
     HTTP_VERSION version_;
     std::string content_;/*报文主体*/
-
-    std::unordered_map<HTTP_HEADER,std::string,EnumClassHash> headerMap_;/*保存各个首部字段*/
+public:
+    std::unordered_map<HTTP_HEADER,std::string,EnumClassHash> headerToStringMap_;/*保存各个首部字段*/
 };
 #endif //RUNNINGBEEF_WEBSERVER_HTTPREQUEST_H
