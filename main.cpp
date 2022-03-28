5d004cbec37de2130c75e717ea60199c7b705adc
RunningBeef <2723772192@qq.com>
2022-03-28 14:51:43 +0800

    运行WebServer,待调试

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
-
+#include "include/WebServer.h"
+class WebServer;
 int main() {
+    WebServer webServer;
+    webServer.run();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
