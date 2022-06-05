48de7e4d650f1c678864fee7474c4ef992330bc7
RunningBeef <2723772192@qq.com>
2022-06-05 21:00:21 +0800

    add select of run_in_backstage

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ bool daemonize()
     umask(0);
     /* 创建新会话，将本进程作为进程组的首领 */
     pid_t sid = setsid();
-    if(sid < 0)
+    if (sid < 0)
     {
         return false;
     }
@@ -25,26 +25,35 @@ bool daemonize()
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
     /* 将标准输入、输出、错误设备重定向到到/dev/null文件 */
-    open("/dev/null",O_RDONLY);
-    open("/dev/null",O_WRONLY);
-    open("/dev/null",O_WRONLY);
+    open("/dev/null", O_RDONLY);
+    open("/dev/null", O_WRONLY);
+    open("/dev/null", O_WRONLY);
     return true;
-
 }
 
+void test()
+{
+    WebServer webServer("192.168.199.128", 7777, 4, 1024, 1000);
+    webServer.run();
+    std::cout << "WebServer over!" << std::endl;
+}
 int main(int argc, char **argv)
 {
-    daemonize();
+    // test();
 
-    if (argc < 6)
+    if (argc < 7)
     {
-        std::cout << "usage:" << argv[0] << " ip_address port_number thread_number maxTask_number waitTime" << std::endl;
+        std::cout << "usage:" << argv[0] << " ip_address port_number thread_number maxTask_number waitTime flag_run_in_backstage(1\0)" << std::endl;
         return 1;
     }
 
+    if (argv[6][0] == '1')
+    {
+        daemonize();
+    }
     char *ip = argv[1];
-    int port = atoi(argv[2]), threadNum = atoi(argv[3]), maxTask = atoi(argv[4]),blockTime = atoi(argv[5]);
-    WebServer webServer(ip, port, threadNum, maxTask,blockTime);
+    int port = atoi(argv[2]), threadNum = atoi(argv[3]), maxTask = atoi(argv[4]), blockTime = atoi(argv[5]);
+    WebServer webServer(ip, port, threadNum, maxTask, blockTime);
     webServer.run();
     std::cout << "WebServer over!" << std::endl;
     return 0;
