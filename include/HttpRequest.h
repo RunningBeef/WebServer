74668af436a4d2c98533f68862e2cebb9ad9a685
RunningBeef <2723772192@qq.com>
2022-10-22 11:44:55 +0800

    -

--- include/HttpRequest.h
+++ include/HttpRequest.h
@@ -2,29 +2,35 @@
 #define HTTPREQUEST_H
 #include <string>
 
-enum class METHOD
-{
-      GET = 0,
-      HEAD,
-      POST,
-      PUT,
-      DELETE,
-      PATCH,
-      TRACE,
-      CONNECT
-};
-enum class HttpVersion
-{
-      HTTP_1_0 = 0,
-      HTTP_1_1
-};
-
 class HttpRequest
 {
 public:
-private:
-      std::string method_;
+      enum class METHOD
+      {
+            GET = 0,
+            HEAD,
+            POST,
+            PUT,
+            DELETE,
+            PATCH,
+            TRACE,
+            CONNECT,
+            METHOD_NOT_SUPPORT
+      };
+      enum class HttpVersion
+      {
+            HTTP1_0 = 0,
+            HTTP1_1,
+            VERSION_NOT_SUPPORT
+      };
+      enum class HttpHeader
+      {
+            ACCEPT = 0,
+            
+      } public:
+          private : std::string method_;
       std::string url_;
-      
+      HttpVersion http_version;
+      std::string body;
 };
 #endif
