f86f2d1def937355a593ded0a807924b7f8fb4c5
RunningBeef <2723772192@qq.com>
2022-11-06 21:41:36 +0800

    -

--- include/HttpData.h
+++ include/HttpData.h
@@ -6,13 +6,23 @@
 #include "ParseHttpResponse.h"
 #include "Timer.h"
 #include "Socket.h"
-class HttpData{
+class HttpData
+{
+
 public:
+      HttpData(std::shared_ptr<ClientSocket> client_socket_ptr,
+               std::shared_ptr<HttpRequest> http_request_ptr,
+               std::shared_ptr<HttpResponse> http_response_ptr,
+               std::shared_ptr<ParseHttpRequest> parse_http_request_ptr,
+               std::shared_ptr<ParseHttpResponse> parse_http_response_ptr,
+               std::weak_ptr<TimerNode> timer_ptr,
+               int client_fd) {}
       std::shared_ptr<ClientSocket> client_socket_ptr_;
       std::shared_ptr<HttpRequest> http_request_ptr_;
       std::shared_ptr<HttpResponse> http_response_ptr_;
       std::shared_ptr<ParseHttpRequest> parse_http_request_ptr_;
       std::shared_ptr<ParseHttpResponse> parse_http_response_ptr_;
-      std::weak_ptr<TimerNode<HttpData>> timer_node_ptr_;
+      std::weak_ptr<TimerNode> timer_ptr_;
+      int client_fd_;
 };
 #endif
--- src/HttpData.cc
+++ src/HttpData.cc
@@ -1,2 +1,14 @@
 #include "../include/HttpData.h"
 
+HttpData::HttpData(std::shared_ptr<ClientSocket> client_socket_ptr,
+                   std::shared_ptr<HttpRequest> http_request_ptr,
+                   std::shared_ptr<HttpResponse> http_response_ptr,
+                   std::shared_ptr<ParseHttpRequest> parse_http_request_ptr,
+                   std::shared_ptr<ParseHttpResponse> parse_http_response_ptr,
+                   std::weak_ptr<TimerNode> timer_ptr,
+                   int client_fd)
+    : client_socket_ptr_(client_socket_ptr), http_request_ptr_(http_request_ptr),
+      http_response_ptr_(http_response_ptr), parse_http_request_ptr_(parse_http_request_ptr),
+      parse_http_response_ptr_(parse_http_response_ptr_), timer_ptr_(timer_ptr), client_fd_(client_fd)
+{
+}
